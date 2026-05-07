d06d0387cfe6d7636af75b5b6bc42846ef873c67
jihun <yjhn0410@gmail.com>
2026-05-07 12:21:57 +0900

    [BUILD] tailwind css 적용

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <main class="flex min-h-screen items-center justify-center bg-slate-950 px-6">
+    <h1 class="text-xl font-bold tracking-tight text-white">Apply tailwind css!</h1>
+  </main>
 </template>
 
 <style scoped></style>
