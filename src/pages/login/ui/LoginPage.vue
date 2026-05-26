6cc9aa1864d252e4f61a2fbb49d931438b120f9e
jihun <yjhn0410@gmail.com>
2026-05-26 13:42:19 +0900

    [BUILD] 로그인 리디렉션 주소 설정 변경

--- src/pages/login/ui/LoginPage.vue
+++ src/pages/login/ui/LoginPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import { apiOrigin } from '@/shared/config/api'
 
 const route = useRoute()
 
@@ -17,7 +18,7 @@ const noticeMessage = computed(() => {
 })
 
 function startGoogleLogin(): void {
-  window.location.assign('/api/oauth2/authorization/google')
+  window.location.assign(`${apiOrigin}/api/oauth2/authorization/google`)
 }
 </script>
 
